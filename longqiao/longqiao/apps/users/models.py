a92a52d4f038b6a0323a7edbac66bf2e1e4225c5
yktimes <1162073988@qq.com>
2019-03-13 15:57:58 +0800

    创建用户模型类

---
+++ longqiao/longqiao/apps/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+import random
+# Create your models here.
+class User(AbstractUser):
+    """用户模型类"""
+
+    StudentID = models.IntegerField(primary_key=True,verbose_name='学号')
+    nickname = models.CharField(max_length=60,verbose_name='昵称',default='陇桥%05d'.format(random.randint(0, 99999)),db_index=True)
+    gender = models.CharField(max_length=10,verbose_name='性别')
+    enrollmentDate = models.CharField(max_length=30,verbose_name='入学日期')
+    birthday = models.CharField(max_length=30,verbose_name='出生日期')
+    department= models.CharField(max_length=30,verbose_name='系别')
+    sclass = models.CharField(max_length=50,verbose_name='班级')
+    classes = models.CharField(max_length=20,verbose_name='级别')
+    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
